030558699b07a38d5b2485f66686f99df9f497b5
Sanjeeb <139191138+Sanjeev0329@users.noreply.github.com>
2026-03-24 11:13:06 +0530

    Update app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const geminiApiKey = 'AIzaSyAGntG90ZYP0WgFEvSfGGpYK0W2s2rtpXo';
+  static const geminiApiKey = 'Place your Api key';
   static const _base =
       'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent';
   static String get geminiEndpoint => '$_base?key=$geminiApiKey';
